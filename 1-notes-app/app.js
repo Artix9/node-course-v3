d09f1b10d0c4207310c9672ef93446fa3f86de43
Artix9 <xxxxx01xxxxx@naver.com>
2022-08-16 03:50:13 +0900

    Reading a Note

--- 1-notes-app/app.js
+++ 1-notes-app/app.js
@@ -1,3 +1,4 @@
+const { demandOption } = require("yargs");
 const yargs = require("yargs");
 const notes = require("./notes");
 
@@ -54,8 +55,15 @@ yargs.command({
 yargs.command({
   command: "read",
   describe: "Read a note",
-  handler() {
-    console.log("Reading a note");
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    notes.readNote(argv.title);
   },
 });
 
--- 1-notes-app/notes.js
+++ 1-notes-app/notes.js
@@ -1,16 +1,12 @@
 const fs = require("fs");
 const chalk = require("chalk");
 
-const getNotes = () => {
-  return "Your notes...";
-};
-
 const addNotes = (title, body) => {
   const notes = loadNotes();
 
-  const duplicateNotes = notes.filter((note) => note.title === title);
+  const duplicateNote = notes.find((note) => note.title === title);
 
-  if (duplicateNotes.length === 0) {
+  if (!duplicateNote) {
     notes.push({ title, body });
     saveNotes(notes);
     console.log(chalk.green.inverse("New note added"));
@@ -40,6 +36,19 @@ const listNotes = () => {
   });
 };
 
+const readNote = (title) => {
+  const notes = loadNotes();
+
+  const note = notes.find((note) => note.title === title);
+
+  if (note) {
+    console.log(chalk.inverse(note.title));
+    console.log(note.body);
+  } else {
+    console.log(chalk.red.inverse("Note not found!"));
+  }
+};
+
 const loadNotes = () => {
   try {
     const dataBuffer = fs.readFileSync("notes.json");
@@ -57,4 +66,4 @@ const saveNotes = (notes) => {
   fs.writeFileSync("notes.json", dataJSON);
 };
 
-module.exports = { getNotes, addNotes, removeNote, listNotes };
+module.exports = { addNotes, removeNote, listNotes, readNote };
