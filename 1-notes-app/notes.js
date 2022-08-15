07d5ea58b4f15c5e21ae02e1293afc04a3e5fe9c
Artix9 <xxxxx01xxxxx@naver.com>
2022-08-15 23:00:03 +0900

    Adding a note

---
+++ 1-notes-app/notes.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+
+const getNotes = () => {
+  return "Your notes...";
+};
+
+const addNotes = (title, body) => {
+  const notes = loadNotes();
+
+  const duplicateNotes = notes.filter((note) => note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push({ title, body });
+    saveNotes(notes);
+    console.log("New note added");
+  } else {
+    console.log("Note title taken!");
+  }
+};
+
+const loadNotes = () => {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    const data = JSON.parse(dataJSON);
+
+    return data;
+  } catch (error) {
+    return [];
+  }
+};
+
+const saveNotes = (notes) => {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
+};
+
+module.exports = { getNotes, addNotes };
