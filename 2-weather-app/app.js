e673a6efe15faca2b9815fdfbc7fdf54b6984ee4
Artix9 <xxxxx01xxxxx@naver.com>
2022-08-17 04:57:17 +0900

    Customizing HTTP Requests

--- 2-weather-app/app.js
+++ 2-weather-app/app.js
@@ -1,9 +1,16 @@
 const request = require("request");
+require("dotenv").config();
+const accessKey = process.env.WEATHER_API_ACCESS_KEY;
 
-const url =
-  "http://api.weatherstack.com/current?access_key=763fbd72227d245c84d66de55d43b087&query=37.8267,-122.4233";
+const url = `http://api.weatherstack.com/current?access_key=${accessKey}&query=37.8267,-122.4233&units=f`;
 
-request(url, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+request({ url, json: true }, (error, response) => {
+  const currentWeather = response.body.current;
+  const temp = currentWeather.temperature;
+  const feelslike = currentWeather.feelslike;
+  const weatherDescriptions = currentWeather.weather_descriptions[0];
+
+  console.log(
+    `${weatherDescriptions}. It is currently ${temp} degrees out. It feels like ${feelslike} degrees out`
+  );
 });
