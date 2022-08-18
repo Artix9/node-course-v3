1bb8e65aac951e4fb4e78fabcf7578d3abad2797
Artix9 <xxxxx01xxxxx@naver.com>
2022-08-19 02:38:49 +0900

    Handling Errors

--- 2-weather-app/app.js
+++ 2-weather-app/app.js
@@ -1,28 +1,37 @@
 const request = require("request");
 require("dotenv").config();
-const accessKey = process.env.WEATHER_API_ACCESS_KEY;
+const weatherApiAccessKey = process.env.WEATHER_API_ACCESS_KEY;
+const geoCodeApiAccessKey = process.env.GEOCODE_API_ACCESS_KEY;
 
-const url = `http://api.weatherstack.com/current?access_key=${accessKey}&query=37.8267,-122.4233&units=f`;
+// const weatherURL = `http://api.weatherstack.com/current?access_key=${weatherApiAccessKey}&query=37.8267,-122.4233&units=f`;
 
-request({ url, json: true }, (error, response) => {
-  const currentWeather = response.body.current;
-  const temp = currentWeather.temperature;
-  const feelslike = currentWeather.feelslike;
-  const weatherDescriptions = currentWeather.weather_descriptions[0];
+// request({ url:weatherURL, json: true }, (error, response) => {
+//   if (error) {
+//     console.log("Unable to connect to weather service");
+//   } else if (response.body.error) {
+//     console.log("Unable to find location");
+//   } else {
+//     const currentWeather = response.body.current;
+//     const temp = currentWeather.temperature;
+//     const feelslike = currentWeather.feelslike;
+//     const weatherDescriptions = currentWeather.weather_descriptions[0];
 
-  console.log(
-    `${weatherDescriptions}. It is currently ${temp} degrees out. It feels like ${feelslike} degrees out`
-  );
-});
+//     console.log(
+//       `${weatherDescriptions}. It is currently ${temp} degrees out. It feels like ${feelslike} degrees out`
+//     );
+//   }
+// });
 
-// Geocoding
-// Address -> Lat/Long -> Weather
-
-const geocodeURL =
-  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1";
+const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${geoCodeApiAccessKey}&limit=1`;
 
 request({ url: geocodeURL, json: true }, (error, response) => {
-  const latitude = response.body.features[0].center[1];
-  const longitude = response.body.features[0].center[0];
-  console.log(latitude, longitude);
+  if (error) {
+    console.log("Unable to connect to location services");
+  } else if (response.body.features.length === 0) {
+    console.log("Unable to find location. Try another search.");
+  } else {
+    const latitude = response.body.features[0].center[1];
+    const longitude = response.body.features[0].center[0];
+    console.log(latitude, longitude);
+  }
 });
