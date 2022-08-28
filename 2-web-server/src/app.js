7829c673567926b370a4ae89ff5f27b2220c3061
Artix9 <xxxxx01xxxxx@naver.com>
2022-08-28 23:37:54 +0900

    The Query String

--- 2-web-server/src/app.js
+++ 2-web-server/src/app.js
@@ -40,7 +40,30 @@ app.get("/help", (req, res) => {
 });
 
 app.get("/weather", (req, res) => {
-  res.send([{ forecst: "It is raining" }, { location: "Boston" }]);
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address",
+    });
+  }
+
+  res.send({
+    forecast: "It is snowing",
+    location: "Boston",
+    address: req.query.address,
+  });
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term",
+    });
+  }
+
+  console.log(req.query.search);
+  res.send({
+    products: [],
+  });
 });
 
 app.get("/help/*", (req, res) => {
