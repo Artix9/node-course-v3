a332e169423f5d8cbbc774a71a450b5697bec4e6
Artix9 <xxxxx01xxxxx@naver.com>
2022-09-05 18:40:55 +0900

    Promises

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -1,24 +1,14 @@
-// const geocode = (address, callback) => {
-//   setTimeout(() => {
-//     const data = {
-//       latitude: 0,
-//       longitude: 0,
-//     };
-
-//     callback(data);
-//   }, 2000);
-// };
-
-// geocode("washington", (data) => {
-//   console.log(data);
-// });
-
-const add = (a, b, callback) => {
+const doWorkCallback = (callback) => {
   setTimeout(() => {
-    callback(a + b);
+    // callback("This is my error!", undefined);
+    callback(undefined, [1, 4, 7]);
   }, 2000);
 };
 
-add(1, 4, (sum) => {
-  console.log(sum); // Should print: 5
+doWorkCallback((error, result) => {
+  if (error) {
+    return console.log(error);
+  }
+
+  console.log(result);
 });
