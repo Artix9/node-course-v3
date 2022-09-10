9976f34b8fbaf6e55af8f1b48c899ffeb97bed8e
Artix9 <xxxxx01xxxxx@naver.com>
2022-09-11 04:56:01 +0900

    Resource Creation Endpoints: Part II

--- 3-task-manager/src/db/mongoose.js
+++ 3-task-manager/src/db/mongoose.js
@@ -1,29 +1,3 @@
 const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api");
-
-const Task = mongoose.model("Task", {
-  description: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-});
-
-const task = new Task({
-  description: "Learn the Mongoose library",
-  completed: false,
-});
-
-task
-  .save()
-  .then(() => {
-    console.log(task);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
