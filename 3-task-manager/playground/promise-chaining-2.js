2a2c4ecd462044a8db8cac4a1a91554f3af706b1
Artix9 <xxxxx01xxxxx@naver.com>
2022-09-16 02:42:50 +0900

    Async/Await: Part II

--- 3-task-manager/playground/promise-chaining-2.js
+++ 3-task-manager/playground/promise-chaining-2.js
@@ -1,13 +1,27 @@
 require("../src/db/mongoose");
 const Task = require("../src/models/task");
 
-Task.findByIdAndDelete("631b3741c4b30f462d8a4da8")
-  .then((task) => {
-    console.log(task);
-    return Task.countDocuments({ completed: false });
-  })
-  .then((result) => {
-    console.log(result);
+// Task.findByIdAndDelete("631b3741c4b30f462d8a4da8")
+//   .then((task) => {
+//     console.log(task);
+//     return Task.countDocuments({ completed: false });
+//   })
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((e) => {
+//     console.log(e);
+//   });
+
+const deleteTaskAndCount = async (id) => {
+  const task = await Task.findByIdAndDelete(id);
+  const count = await Task.countDocuments({ completed: false });
+  return count;
+};
+
+deleteTaskAndCount("631b3741c4b30f462d8a4da8")
+  .then((count) => {
+    console.log(count);
   })
   .catch((e) => {
     console.log(e);
