04096abc09510667dcc04cc79dcace1f62bfbfd6
Artix9 <xxxxx01xxxxx@naver.com>
2022-09-21 22:41:37 +0900

    Securely Storing Passwords: Part I

--- 3-task-manager/src/index.js
+++ 3-task-manager/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("./db/mongoose");
+const bcrypt = require("bcryptjs");
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 
@@ -13,3 +14,16 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
+
+const myFunction = async () => {
+  const password = "mypassword123";
+  const hashedPassword = await bcrypt.hash(password, 8);
+
+  console.log(password);
+  console.log(hashedPassword);
+
+  const isMatch = await bcrypt.compare("mypassword123", hashedPassword);
+  console.log(isMatch);
+};
+
+myFunction();
